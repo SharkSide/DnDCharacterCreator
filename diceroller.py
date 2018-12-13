5b3d7e350fcc4dc7d76d3e51d0e24b81167eb5ff
superspeeder <andy@delusion.org>
2018-12-13 12:42:00 -0800

    finish diceroller.py

--- diceroller.py
+++ diceroller.py
@@ -1,31 +1,36 @@
 from random import randint
 
-results = []
+def roll():
+  results = []
 
-for x in range(6):
-  result = sum([randint(2,7) for y in range(4)])
-  if result > 19:
-    while result > 19:
-      result = sum([randint(2,7) for y in range(4)])
+  for x in range(6):
+    result = sum([randint(2,7) for y in range(4)])
+    if result > 19:
+      while result > 19:
+        result = sum([randint(2,7) for y in range(4)])
 
-  results.append(result)
-  print("You rolled a {}".format(result))
+    results.append(result)
+    print("You rolled a {}".format(result))
 
-print(results)
+  attribs = {0:"strength", 1:"dexterity", 2:"constitution", 3:"intelligence", 4:"wisdom", 5:"charisma"}
 
-attribs = {0:"strength", 1:"dexterity", 2:"constitution", 3:"intelligence", 4:"wisdom", 5:"charisma"}
-print("Available Attributes:")
-for x, a in attribs.items():
-  print("{0}: {1}".format(x, a))
+  print("="*50)
+  print("Available Attributes:")
+  for x, a in attribs.items():
+    print("{0}: {1}".format(x, a))
 
-end_attribs = {}
+  end_attribs = {}
 
-for n in results:
-  print("What attribute do you wish to put {} into".format(n), end=": ")
-  atrb = input("")
-  if attribs[int(atrb)] in end_attribs:
-    print("choose again")
-  end_attribs[attribs[int(atrb)]] = n
+  print("="*50)
+  for n in results:
+    print("What attribute do you wish to put {} into".format(n), end=": ")
+    atrb = input("")
+    if attribs[int(atrb)] in end_attribs:
+      print("choose again")
+    end_attribs[attribs[int(atrb)]] = n
 
-print(n)
+  print("="*50)
+  for name, value in end_attribs.items():
+    print(name+":", value)
 
+  return end_attribs
