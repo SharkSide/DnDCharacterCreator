fee42b593474f7aa26ae3c950f6f97e1b8220e8c
SharkSide <45834027+SharkSide@users.noreply.github.com>
2018-12-13 08:53:33 -0800

    Create intro.py
    introduction

---
+++ intro.py
@@ -0,0 +1,112 @@
+r = 0
+while r == 0:
+  print("What race will you be?")
+  race = input()
+  if race == "Dragonborn":
+    print("Dragonbord selected")
+    r = 1
+  elif race == "Dwarf":
+    print("Dwarf selected")
+    r = 1.
+  elif race == "Elf":
+    print("Elf selected")
+    r = 1
+  elif race == "Gnome":
+    print("Gnome selected")
+    r = 1
+  elif race == "Half Elf":
+    print("Half Elf selected")
+    r = 1
+  elif race == "Half Orc":
+    print("Half Orc selected")
+    r = 1
+  elif race == "Halfling":
+    print("Halfling selected")
+    r = 1
+  elif race == "Human":
+    print("Human selected")
+    r = 1
+  elif race == "Tiefling":
+    print("Tiefling selected")
+    r = 1
+  else:
+    print("That is not an accepted race")
+c = 0
+while c == 0:
+  print("What class will you be?")
+  job = input()
+  if job == "Barbarian":
+    print("Barbarian selected")
+    c = 1
+  elif job == "Bard":
+    print("Bard selected")
+    c = 1
+  elif job == "Cleric":
+    print("Cleric selected")
+    c = 1
+  elif job == "Druid":
+    print("Druid selected")
+    c = 1
+  elif job == "Monk":
+    print("Monk selected")
+    c = 1
+  elif job == "Paladin":
+    print("Paladin selected")
+    c = 1
+  elif job == "Ranger":
+    print("Ranger selected")
+    c = 1
+  elif job == "Rogue":
+    print("Rogue selected")
+    c = 1
+  elif job == "Sorcerer":
+    print("Sorcerer selected")
+    c = 1
+  elif job == "Warlock":
+    print("Warlock selected")
+    c = 1
+  elif job == "Wizard":
+    print("Wizard selected")
+    c = 1
+  else:
+    print("That is not an accepted class")
+a = 0
+while a == 0:
+  print("What allignment will you be?")
+  at = input()
+  if at == "Lawfull Good":
+    print("Lawfull Good selected")
+    a = 1
+  elif job == "Neutral Good":
+    print("Neutral Good selected")
+    a = 1
+  elif job == "Chaotic Good":
+    print("Chaotic Good selected")
+    a = 1
+  #StoppedHere ^^^^^
+  elif job == "Druid":
+    print("Druid selected")
+    c = 1
+  elif job == "Monk":
+    print("Monk selected")
+    c = 1
+  elif job == "Paladin":
+    print("Paladin selected")
+    c = 1
+  elif job == "Ranger":
+    print("Ranger selected")
+    c = 1
+  elif job == "Rogue":
+    print("Rogue selected")
+    c = 1
+  elif job == "Sorcerer":
+    print("Sorcerer selected")
+    c = 1
+  elif job == "Warlock":
+    print("Warlock selected")
+    c = 1
+  elif job == "Wizard":
+    print("Wizard selected")
+    c = 1
+  else:
+    print("That is not an accepted class")
