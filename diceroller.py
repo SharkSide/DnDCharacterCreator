4fb4352d2672870fb7025dbd3c4c0b2efe9c0893
superspeeder <andy@delusion.org>
2018-12-13 08:57:04 -0800

    Update diceroller.py

--- diceroller.py
+++ diceroller.py
@@ -2,12 +2,30 @@ from random import randint
 
 results = []
 
-for x in  range(int(input('How many dicesets: '))):
-  diceset = []
-  for y in range(4):
-    diceset.append(randint(2,7))
+for x in range(6):
+  result = sum([randint(2,7) for y in range(4)])
+  if result > 19:
+    while result > 19:
+      result = sum([randint(2,7) for y in range(4)])
 
-  result = sum(diceset)
   results.append(result)
   print("You rolled a {}".format(result))
+
+print(results)
+
+attribs = {0:"strength", 1:"dexterity", 2:"constitution", 3:"intelligence", 4:"wisdom", 5:"charisma"}
+print("Available Attributes:")
+for x, a in attribs.items():
+  print("{0}: {1}".format(x, a))
+
+end_attribs = {}
+
+for n in results:
+  print("What attribute do you wish to put {} into".format(n), end=": ")
+  atrb = input("")
+  if attribs[int(atrb)] in end_attribs:
+    print("choose again")
+  end_attribs[attribs[int(atrb)]] = n
+
+print(n)
 
