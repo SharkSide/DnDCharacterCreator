127f0800801e87622363441bd23d1d6d25bc8571
SharkSide <45834027+SharkSide@users.noreply.github.com>
2018-12-13 13:00:35 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,7 +1,7 @@
 r = 0
 while r == 0:
   print("What race will you be?")
-  race = input()
+  race = input().capitalize()
   if race == "Dragonborn":
     print("Dragonbord selected")
     r = 1
@@ -34,7 +34,7 @@ while r == 0:
 c = 0
 while c == 0:
   print("What class will you be?")
-  job = input()
+  job = input().capitalize()
   if job == "Barbarian":
     print("Barbarian selected")
     c = 1
@@ -77,36 +77,48 @@ while a == 0:
   if at == "Lawfull Good":
     print("Lawfull Good selected")
     a = 1
-  elif job == "Neutral Good":
+  elif at == "Neutral Good":
     print("Neutral Good selected")
     a = 1
   elif job == "Chaotic Good":
     print("Chaotic Good selected")
     a = 1
-  #StoppedHere ^^^^^
-  elif job == "Druid":
-    print("Druid selected")
-    c = 1
-  elif job == "Monk":
-    print("Monk selected")
-    c = 1
-  elif job == "Paladin":
-    print("Paladin selected")
-    c = 1
-  elif job == "Ranger":
-    print("Ranger selected")
-    c = 1
-  elif job == "Rogue":
-    print("Rogue selected")
-    c = 1
-  elif job == "Sorcerer":
-    print("Sorcerer selected")
-    c = 1
-  elif job == "Warlock":
-    print("Warlock selected")
-    c = 1
-  elif job == "Wizard":
-    print("Wizard selected")
-    c = 1
+  elif at == "Lawfull Neutral":
+    print("Lawfull Neutral selected")
+    a = 1
+  elif at == "Neutral":
+    print("Neutral selected")
+    a = 1
+  elif at == "Chaotic Neutral":
+    print("Chaotic Neutral selected")
+    a = 1
+  elif at == "Lawfull Evil":
+    print("Lawfull Evil selected")
+    a = 1
+  elif at == "Neutral Evil":
+    print("Neutral Evil selected")
+    a = 1
+  elif at == "Chaotic Evil":
+    print("Chaotic Evil selected")
+    a = 1
   else:
-    print("That is not an accepted class")
+    print("That is not an accepted allignment")
+l = 0
+
+def numeric(inp):
+  try: int(inp)
+  except: return False
+  return True
+
+while l == 0:
+  print("What level is the character?")
+  level = input()
+  if numeric(level):
+    if int(level)> 0:
+      print("Character Level", level)
+      l = 1
+    else:
+      print("That is not a level")
+  
+  else:
+    print("That is not a level")
